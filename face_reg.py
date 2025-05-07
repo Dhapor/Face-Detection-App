7a983d1d25e43f58b9c81eacbd5bceb489fc4890
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2025-05-07 18:04:44 +0100

    Update face_reg.py

--- face_reg.py
+++ face_reg.py
@@ -18,7 +18,6 @@ st.markdown("""
             font-size: 40px;
             font-weight: bold;
             text-align: center;
-            text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
         }
         .subheader {
             font-size: 20px;
