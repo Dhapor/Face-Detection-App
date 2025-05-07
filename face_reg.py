f0f207d80ce2b6ec8fc37fa4183dcb4df355e6fa
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2025-05-07 18:16:42 +0100

    Update face_reg.py

--- face_reg.py
+++ face_reg.py
@@ -70,7 +70,7 @@ class FacePalmDetector(VideoTransformerBase):
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
-            cv2.putText(img, "Face", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
+            cv2.putText(img, "Datapsalm", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
 
         # Palm detection using MediaPipe
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
